8a14e02e788e9d503304e4fd7c908da7653f83c4
ablaspinto <ablaspinto@gmail.com>
2025-11-28 23:44:06 -0500

    updated main function, into loop, still need to add full game logic, added error handling

--- src/main.go
+++ src/main.go
@@ -2,7 +2,19 @@ package src
 
 import (
 	"LRZ/src/player"
+	"fmt"
 )
 
 func main() {
+	for {
+		counter, leftOption, rightOption := player.HandleInput()
+		left, right := player.CreatePlayers(leftOption, rightOption)
+		if player.ProperOption(rightOption) && player.ProperOption(leftOption) {
+			player.Dec(left, counter)
+			// logic
+		} else {
+			fmt.Printf("Left or Right have Invalid Options\n")
+			fmt.Printf("Options must be > 0 and <= 10\n")
+		}
+	}
 }

0a2ab63f71df1d7d8eda4aaa6fa36aee9f7cbfb4
ablaspinto <ablaspinto@gmail.com>
2025-11-29 22:05:49 -0500

    have first subtraction logic, as well as some formatting for initial subtraction

--- src/main.go
+++ src/main.go
@@ -1,17 +1,29 @@
-package src
+package main
 
 import (
 	"LRZ/src/player"
 	"fmt"
+	"strconv"
 )
+
+const Reset string = "\033[0m"
+const Red string = "\033[31m"
+const Blue string = "\033[34m"
 
 func main() {
 	for {
 		counter, leftOption, rightOption := player.HandleInput()
+		var original int = counter
 		left, right := player.CreatePlayers(leftOption, rightOption)
 		if player.ProperOption(rightOption) && player.ProperOption(leftOption) {
-			player.Dec(left, counter)
-			// logic
+			fmt.Printf(strconv.Itoa(original) + Blue + " -L-> " + Reset)
+			counter = player.Dec(left, counter)
+			countString := strconv.Itoa(counter)
+			fmt.Printf(countString + Red + "-R->" + Reset)
+			counter = player.Dec(right, counter)
+			rcountString := strconv.Itoa(counter)
+			fmt.Printf(" " + rcountString)
+
 		} else {
 			fmt.Printf("Left or Right have Invalid Options\n")
 			fmt.Printf("Options must be > 0 and <= 10\n")

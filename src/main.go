48fddfb580626418eefc14bd8bd30fee951b637e
ablaspinto <ablaspinto@gmail.com>
2025-12-01 14:41:26 -0500

    main logic, started, game works, however need to now make it so options change , and need to implment system to track wins for players

--- src/main.go
+++ src/main.go
@@ -3,27 +3,32 @@ package main
 import (
 	"LRZ/src/player"
 	"fmt"
-	"strconv"
 )
 
 const Reset string = "\033[0m"
-const Red string = "\033[31m"
-const Blue string = "\033[34m"
 
 func main() {
 	for {
 		counter, leftOption, rightOption := player.HandleInput()
+		var left, right player.Player
 		var original int = counter
-		left, right := player.CreatePlayers(leftOption, rightOption)
+		left, right = player.CreatePlayers(leftOption, rightOption)
 		if player.ProperOption(rightOption) && player.ProperOption(leftOption) {
-			fmt.Printf(strconv.Itoa(original) + Blue + " -L-> " + Reset)
-			counter = player.Dec(left, counter)
-			countString := strconv.Itoa(counter)
-			fmt.Printf(countString + Red + "-R->" + Reset)
-			counter = player.Dec(right, counter)
-			rcountString := strconv.Itoa(counter)
-			fmt.Printf(" " + rcountString)
-
+			fmt.Printf("%d", original)
+			for {
+				counter = player.Dec(left, counter)
+				if counter < 0 {
+					fmt.Println("Right Wins!\n")
+					break
+				}
+				fmt.Printf(player.PrintFunction(counter, left))
+				counter = player.Dec(right, counter)
+				if counter < 0 {
+					fmt.Println("\nLeft Wins!\n")
+					break
+				}
+				fmt.Printf(player.PrintFunction(counter, right))
+			}
 		} else {
 			fmt.Printf("Left or Right have Invalid Options\n")
 			fmt.Printf("Options must be > 0 and <= 10\n")

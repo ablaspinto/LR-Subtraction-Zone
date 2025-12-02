71b4f9a667fa2c551ae41f1cf94b558a4b0c5774
ablaspinto <ablaspinto@gmail.com>
2025-12-01 23:53:56 -0500

    added map, to keep track of counter, and which player won with which options, need to add loop to go through games

--- src/main.go
+++ src/main.go
@@ -5,30 +5,23 @@ import (
 	"fmt"
 )
 
-const Reset string = "\033[0m"
+var listOfWinners []map[int]player.Player
 
 func main() {
 	for {
 		counter, leftOption, rightOption := player.HandleInput()
-		var left, right player.Player
 		var original int = counter
-		left, right = player.CreatePlayers(leftOption, rightOption)
+		var left, right = player.CreatePlayers(leftOption, rightOption)
 		if player.ProperOption(rightOption) && player.ProperOption(leftOption) {
 			fmt.Printf("%d", original)
 			for {
-				counter = player.Dec(left, counter)
-				if counter < 0 {
-					fmt.Println("Right Wins!\n")
-					break
-				}
-				fmt.Printf(player.PrintFunction(counter, left))
-				counter = player.Dec(right, counter)
-				if counter < 0 {
-					fmt.Println("\nLeft Wins!\n")
-					break
-				}
-				fmt.Printf(player.PrintFunction(counter, right))
+				playerMap := make(map[int]player.Player)
+				var num, player = player.PlayAgain(left, right, counter)
+				playerMap[num] = player
+				listOfWinners = append(listOfWinners, playerMap)
+				break
 			}
+			fmt.Printf("%v\n", listOfWinners)
 		} else {
 			fmt.Printf("Left or Right have Invalid Options\n")
 			fmt.Printf("Options must be > 0 and <= 10\n")

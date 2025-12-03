2dff5c82e22c6619efb2e6dbcb1b393fbcf612ba
ablaspinto <ablaspinto@gmail.com>
2025-12-02 21:16:47 -0500

    go back to previous commit if you want to play game manually, main function just shows all player moves and the array of counters

--- src/main.go
+++ src/main.go
@@ -8,23 +8,7 @@ import (
 var listOfWinners []map[int]player.Player
 
 func main() {
-	for {
-		counter, leftOption, rightOption := player.HandleInput()
-		var original int = counter
-		var left, right = player.CreatePlayers(leftOption, rightOption)
-		if player.ProperOption(rightOption) && player.ProperOption(leftOption) {
-			fmt.Printf("%d", original)
-			for {
-				playerMap := make(map[int]player.Player)
-				var num, player = player.PlayAgain(left, right, counter)
-				playerMap[num] = player
-				listOfWinners = append(listOfWinners, playerMap)
-				break
-			}
-			fmt.Printf("%v\n", listOfWinners)
-		} else {
-			fmt.Printf("Left or Right have Invalid Options\n")
-			fmt.Printf("Options must be > 0 and <= 10\n")
-		}
-	}
+	counters, allPlayerMoves := player.GetAllNumbers()
+	fmt.Printf("%v", counters)
+	fmt.Printf("%v", allPlayerMoves)
 }

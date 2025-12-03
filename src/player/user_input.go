3b4b8a9aa9a553e0dbfe290e3ab9352750c43de9
ablaspinto <ablaspinto@gmail.com>
2025-12-02 21:18:07 -0500

    added function to get all of the information, that the user needs in order to solve who wins which games on SLRZ

--- src/player/user_input.go
+++ src/player/user_input.go
@@ -1,6 +1,8 @@
 package player
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func ProperOption(num int) bool {
 	return num > 0 && num <= 10
@@ -17,4 +19,24 @@ func HandleInput() (int, int, int) {
 	fmt.Printf("What would you like Right's option to be?\n")
 	fmt.Scan(&rightOption)
 	return counters, leftOption, rightOption
+}
+
+func GetAllNumbers() ([100]int, map[string][]Player) {
+	var intArr [100]int
+	var playerMoves = make(map[string][]Player)
+	playerMoves["L"] = []Player{}
+	playerMoves["R"] = []Player{}
+	var j int = 0
+	for i := 1; i < 101; i++ {
+		intArr[j] = i
+		j++
+	}
+	for i := 1; i < 11; i++ {
+		for j := 1; j < 11; j++ {
+			left, right := CreatePlayers(i, j)
+			playerMoves["L"] = append(playerMoves["L"], left)
+			playerMoves["R"] = append(playerMoves["R"], right)
+		}
+	}
+	return intArr, playerMoves
 }

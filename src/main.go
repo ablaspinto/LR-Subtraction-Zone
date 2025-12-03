5934970ce126f2d10f8dfd2e6d9d1305aa496ec1
ablaspinto <ablaspinto@gmail.com>
2025-12-03 16:59:25 -0500

    updated main function, shows LGF and RGF maps, and what they won on

--- src/main.go
+++ src/main.go
@@ -5,10 +5,27 @@ import (
 	"fmt"
 )
 
-var listOfWinners []map[int]player.Player
+var leftGoingFirst []map[int]player.Player
+var rightGoingFirst []map[int]player.Player
 
 func main() {
 	counters, allPlayerMoves := player.GetAllNumbers()
-	fmt.Printf("%v", counters)
-	fmt.Printf("%v", allPlayerMoves)
+	leftPlayerMoves := allPlayerMoves["L"]
+	rightPlayerMoves := allPlayerMoves["R"]
+	// crosss reference maps
+	for i := 0; i < len(counters); i++ {
+		currLeftMove := leftPlayerMoves[i]
+		currRightMove := rightPlayerMoves[i]
+		countNumber, lgfWinners := player.PlayAgain(currLeftMove, currRightMove, counters[i])
+		countNumber, rgfWinners := player.PlayAgain(currRightMove, currLeftMove, counters[i])
+		m1 := make(map[int]player.Player)
+		m2 := make(map[int]player.Player)
+		m1[countNumber] = lgfWinners
+		m2[countNumber] = rgfWinners
+		leftGoingFirst = append(leftGoingFirst, m1)
+		rightGoingFirst = append(rightGoingFirst, m2)
+	}
+	fmt.Printf("%v\n", rightGoingFirst)
+	fmt.Printf("%v\n", leftGoingFirst)
+
 }
